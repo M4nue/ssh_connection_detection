2165302e4a078ea1646ebb83ebf16e475383cd4c
M4nue <mrodcur12042005@gmail.com>
2024-12-19 20:24:48 +0100

    Modificaciones leves y detección de accesos por ssh

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-#Autores: M4nue y f1rul4yx
+#Autores: M4nue
 #Descripción: Script para informar cuando alguien se ha conectado a tu maquina, actuando como servicio
 
-#Variables globales
-IPS_PERMITIDAS=$($(ip a | awk -F " " '/([1-9]{1,3}\.){3}[1-9]{1,3}/{print $2,$4}' | awk -F "/" '{print $1,$2}' | awk -F " " '{print $1,$3}' | tr -s "[:space:]" ":"))
+#Variables
+IPS_PERMITIDAS=$(ip a | awk -F " " '/([0-9]{1,3}\.){3}[0-9]{1,3}/{print $2,$4}' | awk -F "/" '{print $1,$2}' | awk -F " " '{print $1,$3}' | tr -s "[:space:]" ":")
+DETECCION_CONEXIONES=$(journalctl -u ssh -o json | jq | egrep "MESSAGE\"\:\ \"Accepted" | awk -F " " '{print $7,$9}')
 
+
+
+echo -e "$IPS_PERMITIDAS \n $DETECCION_CONEXIONES"
